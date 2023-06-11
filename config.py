44fee35d043cc0fc8c9606099391522bd98079a8
Luis García Ramos <luisgarciar@users.noreply.github.com>
2023-06-11 16:00:54 +0200

    add config file and rename validation folder

---
+++ config.py
@@ -0,0 +1,8 @@
+# specify path to the training and validation data
+TRAIN = "data/train"
+VAL = "data/val"
+# set the input height and width
+INPUT_HEIGHT = 256
+INPUT_WIDTH = 256
+# set the batch size and validation data split
+BATCH_SIZE = 5
